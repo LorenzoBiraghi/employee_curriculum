0cda7b3da137e44dca0f50fdc9d197944ee8a29e
biraghi <biraghilorenzo02@gmail.com>
2022-05-05 11:36:51 +0200

    Initial commit

---
+++ src/main/java/com/vidiemme/employee_curriculum/EmployeeCurriculumApplication.java
@@ -0,0 +1,13 @@
+package com.vidiemme.employee_curriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCurriculumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidiemme/employee_curriculum/EmployeeCurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidiemme.employee_curriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
